fce947c173636425ca2e5fa151dcfad9364bd09e
Sergey Kapralov <ser-kapralov@yandex.ru>
2022-10-25 10:58:36 +0300

    Created user in cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world!")
+	someUser := models.User{
+		ID:      1,
+		Name:    "John",
+		Surname: "Doe",
+		Age:     42,
+		Weight:  81.7,
+	}
+
+	fmt.Println(someUser)
 }
