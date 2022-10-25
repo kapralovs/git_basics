00ea50f5439a25d848276c3e7e53218d976e3657
Sergey Kapralov <ser-kapralov@yandex.ru>
2022-10-25 11:13:17 +0300

    User creation is commented out, changed greeting

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world!")
-	someUser := models.User{
-		ID:      1,
-		Name:    "John",
-		Surname: "Doe",
-		Age:     42,
-		Weight:  81.7,
-	}
+	fmt.Println("hello world with modules!")
+	// someUser := models.User{}
 
-	fmt.Println(someUser)
+	// fmt.Println(someUser)
 }
